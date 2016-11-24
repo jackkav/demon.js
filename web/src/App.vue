895b9f7c6e6869450ab44b5b0a55c38fe068e198
Jack Kavanagh <jackkav@gmail.com>
2016-11-24 22:17:17 +0800

    added element ui

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <h1>{{ msg }}</h1>
+    <el-button @click.native="startHacking">Let's do it</el-button>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
-
 export default {
-  name: 'app',
-  components: {
-    Hello,
+  data() {
+    return {
+      msg: 'Use Vue 2.0 Today!',
+    }
   },
-};
+  methods: {
+    startHacking() {
+      this.$notify({
+        title: 'It Works',
+        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
+        duration: 6000,
+      })
+    }
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: Helvetica, sans-serif;
 }
 </style>
