cdde7a9c1c81808dc99ad2857b9e910566c061ac
Jack Kavanagh <jackkav@gmail.com>
2016-11-25 15:07:05 +0800

    comment out search

--- web/src/components/TorrentTable.vue
+++ web/src/components/TorrentTable.vue
@@ -25,9 +25,7 @@ export default {
         duration: 6000
       }
       this.$notify(data)
-        // to show twk
-      location.href =
-        'magnet:?xt=urn:btih:c8519027996ef2a57d49cc81d76bd708646a829f&dn=The.Mindy.Project.S05E05.HDTV.x264-KILLERS%5Beztv%5D.mkv%5Beztv%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A80&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
+      location.href = val.magnet
     }
   },
   data() {
@@ -39,28 +37,32 @@ export default {
         name: 'Game of Thrones S01E01 REPACK HDTV x264-MiNDTHEGAP [eztv]',
         address: 'No. 189, Grove St, Los Angeles',
         size: '100 MB',
-        seeds: '10'
+        seeds: '10',
+        magnet: 'magnet:?xt=urn:btih:c8519027996ef2a57d49cc81d76bd708646a829f&dn=The.Mindy.Project.S05E05.HDTV.x264-KILLERS%5Beztv%5D.mkv%5Beztv%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A80&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
       }, {
         date: '2016-05-03',
         released: moment('2016-05-03').fromNow(),
         name: 'The Walking Dead S01E01 REPACK HDTV x264-MiNDTHEGAP [eztv]',
         address: 'No. 189, Grove St, Los Angeles',
         size: '100 MB',
-        seeds: '10'
+        seeds: '10',
+        magnet: 'magnet:?xt=urn:btih:c8519027996ef2a57d49cc81d76bd708646a829f&dn=The.Mindy.Project.S05E05.HDTV.x264-KILLERS%5Beztv%5D.mkv%5Beztv%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A80&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
       }, {
         date: '2016-04-03',
         released: moment('2016-04-03').fromNow(),
         name: 'Tosh 0 S08E29 REPACK HDTV x264-MiNDTHEGAP [eztv]',
         address: 'No. 189, Grove St, Los Angeles',
         size: '100 MB',
-        seeds: '10'
+        seeds: '10',
+        magnet: 'magnet:?xt=urn:btih:c8519027996ef2a57d49cc81d76bd708646a829f&dn=The.Mindy.Project.S05E05.HDTV.x264-KILLERS%5Beztv%5D.mkv%5Beztv%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A80&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
       }, {
         date: '2016-02-03',
         released: moment('2016-02-03').fromNow(),
         name: 'James Corden 2016 11 22 Aaron Eckhart HDTV x264-CROOKS [eztv]',
         address: 'No. 189, Grove St, Los Angeles',
         size: '100 MB',
-        seeds: '10'
+        seeds: '10',
+        magnet: 'magnet:?xt=urn:btih:c8519027996ef2a57d49cc81d76bd708646a829f&dn=The.Mindy.Project.S05E05.HDTV.x264-KILLERS%5Beztv%5D.mkv%5Beztv%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A80&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
       }]
     }
   }
