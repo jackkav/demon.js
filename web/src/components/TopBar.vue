af9798a33c1141af9e4f4302b79c73f4acad4afe
Jack Kavanagh <jackkav@gmail.com>
2016-11-25 15:08:37 +0800

    actually commented out search

--- web/src/components/TopBar.vue
+++ web/src/components/TopBar.vue
@@ -3,10 +3,10 @@
   <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
     <el-menu-item index="home">Home page</el-menu-item>
     <el-menu-item index="pageTwo">Page two</el-menu-item>
-    <el-menu-item index="search" justify="right">
+    <!-- <el-menu-item index="search" justify="right">
       <el-input placeholder="Search" v-model="input">
       </el-input>
-    </el-menu-item>
+    </el-menu-item> -->
   </el-menu>
 </div>
 </template>
