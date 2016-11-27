39ade764f0c86e87d010c7638fc535060a60b6b0
Jack Kavanagh <jackkav@gmail.com>
2016-11-28 00:00:41 +0800

    pull episode metadata from magnet title

--- scraper/index.js
+++ scraper/index.js
@@ -10,9 +10,15 @@ request({
     var link = $(this);
     var title = link.attr("title");
     var magnet = link.attr("href");
+    var quality = title.match(/720p/g) || "HDTV"
+    var size = title.match(/\(.*\)/g)
+    var episode = title.match(/S\d\dE\d\d/g) || title.match(/\d{4} \d{2} \d{2}/g)
     const data = {
       title,
-      magnet
+      size,
+      episode,
+      quality,
+      //magnet
     }
     console.log(data);
   });
