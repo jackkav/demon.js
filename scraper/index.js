6167700f191dc0df6b659b8ab4e9b64bc5f3d3c4
Jack Kavanagh <jackkav@gmail.com>
2016-11-27 22:28:48 +0800

    added scraping latest 50

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,16 @@
 var request = require("request");
+var cheerio = require("cheerio");
 
 request({
   uri: "http://www.eztv.ag",
 }, function(error, response, body) {
-  console.log(body);
+  var $ = cheerio.load(body);
+
+  $(".epinfo").each(function() {
+    var link = $(this);
+    var text = link.text();
+    var href = link.attr("href");
+
+    console.log(text + " -> " + href);
+  });
 });
