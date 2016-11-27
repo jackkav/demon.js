bd4f09ab866369202d0691c3bf48675a1f81ea81
Jack Kavanagh <jackkav@gmail.com>
2016-11-28 00:19:19 +0800

    cleaned up metadata parsing

--- scraper/index.js
+++ scraper/index.js
@@ -11,13 +11,15 @@ request({
     var title = link.attr("title");
     var magnet = link.attr("href");
     var quality = title.match(/720p/g) || "HDTV"
-    var size = title.match(/\(.*\)/g).replace(/[\(\)']+/g,'')
+    var size = title.match(/\(.*\)/g).toString()
+    if (size) size = size.slice(1,size.length-1)
+    // size = size.replace(/[\(\)']+/g,'')
     var episode = title.match(/S\d\dE\d\d/g) || title.match(/\d{4} \d{2} \d{2}/g)
     const data = {
       title,
       size,
-      episode,
-      quality,
+      episode:episode?episode.toString():"N/A",
+      quality:quality.toString(),
       //magnet
     }
     console.log(data);
