0b4c779de43e0ecfc4920a1f4cb5e4e8ab844bbd
Jack Kavanagh <jackkav@gmail.com>
2016-11-28 00:05:57 +0800

    added gitignore

--- scraper/index.js
+++ scraper/index.js
@@ -11,7 +11,7 @@ request({
     var title = link.attr("title");
     var magnet = link.attr("href");
     var quality = title.match(/720p/g) || "HDTV"
-    var size = title.match(/\(.*\)/g)
+    var size = title.match(/\(.*\)/g).replace(/[\(\)']+/g,'')
     var episode = title.match(/S\d\dE\d\d/g) || title.match(/\d{4} \d{2} \d{2}/g)
     const data = {
       title,
