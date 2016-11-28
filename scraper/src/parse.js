c1fdd3b64f80be4b395bb9c4d3a14c7cda052524
Jack Kavanagh <jackkav@gmail.com>
2016-11-28 19:45:36 +0800

    removed global from regex queries

--- scraper/src/parse.js
+++ scraper/src/parse.js
@@ -1,11 +1,11 @@
 export default (input) => {
-  const e = input.match(/S\d\dE\d\d/g) || input.match(/\d{4} \d{2} \d{2}/g)
+  const e = input.match(/S\d\dE\d\d/) || input.match(/\d{4} \d{2} \d{2}/)
   const episode = e ? e.toString() : ''
 
-  const q = input.match(/720p/g)
+  const q = input.match(/720p/)
   const quality = q ? q.toString() : 'HDTV'
 
-  let size = input.match(/\(.*\)/g).toString()
+  let size = input.match(/\(.*\)/).toString()
   if (size) size = size.slice(1, size.length - 1)
 
   const episodeIndex = input.indexOf(episode)
