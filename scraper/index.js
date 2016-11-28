28a678c20fd1b140a1fa370ef53ace1d0a4a8ec3
Jack Kavanagh <jackkav@gmail.com>
2016-11-28 17:47:35 +0800

    set up persist project and re-arranged dependancies

--- scraper/index.js
+++ scraper/index.js
@@ -13,6 +13,13 @@ request({uri: 'http://www.eztv.ag'}, (error, response, body) => {
     let size = title.match(/\(.*\)/g).toString()
     if (size) size = size.slice(1, size.length - 1)
     const episode = title.match(/S\d\dE\d\d/g) || title.match(/\d{4} \d{2} \d{2}/g)
+
+    // get title from everything before season component?
+    // do i need seeds?
+    // do i need release time?
+
+    // how to fold on quality? - remove 720p and check for equality? REPACK?
+
     const data = {
       title,
       size,
