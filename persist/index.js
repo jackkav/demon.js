d2ff21b81b636e52e02dac6a6d4b4d3586edd00d
Jack Kavanagh <jackkav@gmail.com>
2016-11-28 18:59:46 +0800

    added basic persistance layer which can write a single value

---
+++ persist/index.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
+mongoose.Promise = global.Promise
+
+const Show = mongoose.model('show', { name: String, episode: String, size: String, quality: String, })
+
+const kitty = new Show({ name: 'Shameless US' })
+kitty.save(function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('meow')
+  }
+})
