a76a88b9d064f2ca5055c670850278414cbc1a19
Jack Kavanagh <jackkav@gmail.com>
2016-11-29 09:07:50 +0800

    added notes

--- persist/index.js
+++ persist/index.js
@@ -1,28 +1,41 @@
 var express = require('express')
 var app = express()
 var router = express.Router()
-
+// https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 // TODO: add api route to fetch all db data
 // TODO: connect to mongoose localdb
 // TODO: connect to mongolab
 // TODO: add write to db to method
 
+router.use(function(req, res, next) {
+    // do logging
+  console.log('Something is happening.')
+  next() // make sure we go to the next routes and don't stop here
+})
+
 // REST API
-router.route('/items')
+router.route('/shows')
+// Get all shows
 .get(function(req, res, next) {
   res.send('Get')
 })
+// Add new show
 .post(function(req, res, next) {
+  console.log(req)
+  // mongoose save
   res.send('Post')
 })
 
-router.route('/items/:id')
+router.route('/shows/:id')
+// Get one show
 .get(function(req, res, next) {
   res.send('Get id: ' + req.params.id)
 })
+// Update one show
 .put(function(req, res, next) {
   res.send('Put id: ' + req.params.id)
 })
+// Delete on show
 .delete(function(req, res, next) {
   res.send('Delete id: ' + req.params.id)
 })
