a758edd5af943e6b2981605d17c59cb4e0452204
Jack Kavanagh <jackkav@gmail.com>
2016-11-30 18:43:32 +0800

    finished web set up for api

--- README.md
+++ README.md
@@ -10,5 +10,12 @@ cd persist && npm start
 
 ## Run Scraper
 ```bash
+export API_LOCATION="http://myapi.com/api/"
 cd scraper && npm start
+```
+
+## Run WebUI
+```bash
+export API_LOCATION="http://myapi.com/api/"
+cd web && npm start
 ```
--- web/src/bootstrap.js
+++ web/src/bootstrap.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 Vue.config.debug = process.env.NODE_ENV !== 'production'
 
 import Axios from 'axios'
-Axios.defaults.baseURL = process.env.API_LOCATION || 'http://localhost:3000/api/'
+Axios.defaults.baseURL = process.env.API_LOCATION || 'http://120.131.8.223:49960/api/'
 Axios.defaults.headers.common.Accept = 'application/json'
 Axios.defaults.timeout = 10000
 Vue.prototype.$http = Axios
