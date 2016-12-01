4073e79b7e1b6a0afd2d6ec80a04424ea118c199
Jack Kavanagh <jackkav@gmail.com>
2016-12-01 12:26:42 +0800

    added support for short running series

--- scraper/__tests__/episode.spec.js
+++ scraper/__tests__/episode.spec.js
@@ -3,7 +3,7 @@ import parse from '../src/parse'
 // given 5 episodes with varying structures can pass data collection tests
 // Westworld S01E09 720p HDTV x264-AVS [eztv] (1.07 GB) Magnet Link
 // Shameless US S07E09 HDTV x264-LOL [eztv] (410.63 MB) Magnet Link
-// Planet Earth II S01E04 HDTV x264-BEGUN [eztv] (508.98 MB) Magnet Link
+// Food Unwrapped Series 9 13of18 Pesto 720p HDTV x264 AAC mp4 [eztv] (410.63 MB) Magnet Link
 // Stephen Colbert 2016 11 09 Miles Teller HDTV x264-UAV [eztv] (420.98 MB) Magnet Link
 // NHK Great Nature 2014 The Enigmatic Island Tasmania 720p HDTV x264 AAC mkv [eztv] (1.32 GB) Magnet Link
 test('should pull episode from title', () => {
@@ -11,8 +11,8 @@ test('should pull episode from title', () => {
   expect(parse(input).episode).toBe('S01E09')
 })
 test('should pull different episode from title', () => {
-  const input = 'Shameless US S07E09 HDTV x264-LOL [eztv] (410.63 MB) Magnet Link'
-  expect(parse(input).episode).toBe('S07E09')
+  const input = 'Food Unwrapped Series 9 13of18 Pesto 720p HDTV x264 AAC mp4 [eztv] (410.63 MB) Magnet Link'
+  expect(parse(input).episode).toBe('Series 9 3of8')
 })
 test('should pull quality from title', () => {
   const input = 'Westworld S01E09 720p HDTV x264-AVS [eztv] (1.07 GB) Magnet Link'
--- scraper/src/parse.js
+++ scraper/src/parse.js
@@ -1,5 +1,5 @@
 export default (input) => {
-  const e = input.match(/S\d\dE\d\d/) || input.match(/\d{4} \d{2} \d{2}/)
+  const e = input.match(/S\d\dE\d\d/) || input.match(/\d{4} \d{2} \d{2}/) || input.match(/Series \d \d{1,2}of\d{1,2}/)
   const episode = e ? e.toString() : ''
 
   const q = input.match(/720p/)
