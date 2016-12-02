5673f9626a00892ba63d662d949680f5b2c0b97e
Jack Kavanagh <jackkav@gmail.com>
2016-12-02 15:08:59 +0800

    :

--- web/src/bootstrap.js
+++ web/src/bootstrap.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 Vue.config.debug = process.env.NODE_ENV !== 'production'
 
 import Axios from 'axios'
+
 Axios.defaults.baseURL = process.env.API_LOCATION || 'http://120.131.8.223:49960/api/'
 Axios.defaults.headers.common.Accept = 'application/json'
 Axios.defaults.timeout = 20000
