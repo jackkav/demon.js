41c1912fdef3f90b0f79ce847379da6def8bbc4c
Jack Kavanagh <jackkav@gmail.com>
2016-12-04 19:21:37 +0800

    fixed scraper

--- scraper/src/index.js
+++ scraper/src/index.js
@@ -1,5 +1,5 @@
-// import getLatest from './getLatest'
-import getSeries from './getSeries'
+import getLatest from './getLatest'
+// import getSeries from './getSeries'
 // import schedule from 'node-schedule'
 import Axios from 'axios'
 Axios.defaults.baseURL = process.env.API_LOCATION || 'http://120.131.8.223:49960/api/'
