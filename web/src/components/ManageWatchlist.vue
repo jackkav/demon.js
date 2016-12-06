d13d030162b5facfc3b1262e4927a5318a046121
Jack Kavanagh <jackkav@gmail.com>
2016-12-06 13:37:44 +0800

    improved visit visit user guidance

--- web/src/components/ManageWatchlist.vue
+++ web/src/components/ManageWatchlist.vue
@@ -1,28 +1,36 @@
 <template>
-<el-table :data="watchlistTable" border style="width: 100%">
-  <el-table-column prop="title" label="Name" align="left">
-  </el-table-column>
-  <el-table-column prop="latestRelease" label="Latest Release(TODO)" align="left" width="200">
-  </el-table-column>
-  <el-table-column prop="nextRelease" label="Next Release(TODO)" align="left" width="200">
-  </el-table-column>
-  <el-table-column :context="_self" inline-template width="180">
-    <div>
-      <el-button size="small">
-        Info
-      </el-button>
-      <el-button size="small" type="danger" @click="handleRemove($index, row)">
-        Remove
-      </el-button>
-    </div>
-  </el-table-column>
-</el-table>
+<div>
+  <el-table v-if="hasWatchlist" :data="watchlistTable" border style="width: 100%">
+    <el-table-column prop="title" label="Name" align="left">
+    </el-table-column>
+    <el-table-column prop="latestRelease" label="Latest Release(TODO)" align="left" width="200">
+    </el-table-column>
+    <el-table-column prop="nextRelease" label="Next Release(TODO)" align="left" width="200">
+    </el-table-column>
+    <el-table-column :context="_self" inline-template width="180">
+      <div>
+        <el-button size="small">
+          Info
+        </el-button>
+        <el-button size="small" type="danger" @click="handleRemove($index, row)">
+          Remove
+        </el-button>
+      </div>
+    </el-table-column>
+  </el-table>
+  <div v-else>
+    Add some shows to your watchlist in the filter above
+  </div>
+</div>
 </template>
 <script>
 import moment from 'moment'
 export default {
   data() {
+    let hasWatchlist = false
+    if (localStorage.getItem('demon.liked')) hasWatchlist = true
     return {
+      hasWatchlist,
       likedShows: localStorage.getItem('demon.liked') ? JSON.parse(localStorage.getItem('demon.liked')).sort() : [],
       watchlistTable: JSON.parse(localStorage.getItem('demon.liked')).map((title) => {
         const expectedDate = moment().add(Math.random() * 10, 'days').toISOString()
