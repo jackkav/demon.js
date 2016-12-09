3ded346014ec92ae6a0f62c54461aa684fd6278c
Jack Kavanagh <jackkav@gmail.com>
2016-12-10 01:11:29 +0800

    switched host

--- web/src/bootstrap.js
+++ web/src/bootstrap.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 Vue.config.debug = process.env.NODE_ENV !== 'production'
 import Axios from 'axios'
-Axios.defaults.baseURL = Vue.config.debug ? 'http://localhost:3000/api/' : 'http://120.131.8.223:49960/api/'
+Axios.defaults.baseURL = Vue.config.debug ? 'http://localhost:3000/api/' : 'https://demonic-persistance.herokuapp.com/api/'
 Axios.defaults.headers.common.Accept = 'application/json'
 Axios.defaults.timeout = 20000
 Vue.prototype.$http = Axios
